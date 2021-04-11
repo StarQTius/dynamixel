c6d2544395d65675f1d913ff92fb2e7a1b4b6fee
StarQTius <dussautoirp@gmail.com>
2021-04-11 15:42:44 +0200

    Renommer tests unitaires

--- test/io_operation.hpp
+++ test/io_operation.hpp
@@ -1,7 +1,7 @@
 #include "tool/io_operation.hpp"
 #include <unity.h>
 
-inline void write_and_interpret_in_unaligned_data_little_endian() {
+inline void io_operation_write_and_interpret_little_endian() {
   using namespace dxl::tool;
 
   UnalignedData<4 * sizeof(uint16_t)> unaligned_data{Endianess::little};
@@ -9,7 +9,7 @@ inline void write_and_interpret_in_unaligned_data_little_endian() {
   TEST_ASSERT_EQUAL_UINT16(0xabcd, unaligned_data.interpret_as<uint16_t>(sizeof(uint16_t)));
 }
 
-inline void write_and_interpret_in_unaligned_data_big_endian() {
+inline void io_operation_write_and_interpret_in_unaligned_data_big_endian() {
   using namespace dxl::tool;
 
   UnalignedData<4 * sizeof(uint16_t)> unaligned_data{Endianess::big};
@@ -17,7 +17,7 @@ inline void write_and_interpret_in_unaligned_data_big_endian() {
   TEST_ASSERT_EQUAL_UINT16(0xabcd, unaligned_data.interpret_as<uint16_t>(sizeof(uint16_t)));
 }
 
-inline void set_and_get_in_unaligned_arguments_little_endian() {
+inline void io_operation_set_and_get_in_unaligned_arguments_little_endian() {
   using namespace dxl::tool;
 
   UnalignedArguments<int, char, long, bool> unaligned_arguments{Endianess::little};
@@ -25,7 +25,7 @@ inline void set_and_get_in_unaligned_arguments_little_endian() {
   TEST_ASSERT_EQUAL(0xabcd, unaligned_arguments.get<2>());
 }
 
-inline void set_and_get_in_unaligned_arguments_big_endian() {
+inline void io_operation_set_and_get_in_unaligned_arguments_big_endian() {
   using namespace dxl::tool;
 
   UnalignedArguments<int, char, long, bool> unaligned_arguments{Endianess::big};
@@ -33,7 +33,7 @@ inline void set_and_get_in_unaligned_arguments_big_endian() {
   TEST_ASSERT_EQUAL(0xabcd, unaligned_arguments.get<2>());
 }
 
-inline void iterate_unaligned_data() {
+inline void io_operation_iterate_unaligned_data() {
   using namespace dxl::tool;
 
   byte_t raw_data[] {0xaa, 0xbb, 0xcc};
--- test/main.cpp
+++ test/main.cpp
@@ -8,11 +8,11 @@ void setup() {
   TEST_MESSAGE(dxl::tool::platform_endianess == dxl::tool::Endianess::little ?
     "La platforme utilise le petit-boutisme" : "La platforme utilise le gros-boutisme");
 
-  RUN_TEST(write_and_interpret_in_unaligned_data_little_endian);
-  RUN_TEST(write_and_interpret_in_unaligned_data_big_endian);
-  RUN_TEST(set_and_get_in_unaligned_arguments_little_endian);
-  RUN_TEST(set_and_get_in_unaligned_arguments_big_endian);
-  RUN_TEST(iterate_unaligned_data);
+  RUN_TEST(io_operation_write_and_interpret_in_unaligned_data_little_endian);
+  RUN_TEST(io_operation_write_and_interpret_in_unaligned_data_big_endian);
+  RUN_TEST(io_operation_set_and_get_in_unaligned_arguments_little_endian);
+  RUN_TEST(io_operation_set_and_get_in_unaligned_arguments_big_endian);
+  RUN_TEST(io_operation_iterate_unaligned_data);
 
   UNITY_END();
 }
