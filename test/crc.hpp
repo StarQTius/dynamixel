397f27b9c8a272a6bf5ba360f236f35b334d9f94
StarQTius <dussautoirp@gmail.com>
2021-06-13 02:14:09 +0200

    Add submodules back

--- test/crc.hpp
+++ test/crc.hpp
@@ -5,6 +5,6 @@
 inline void crc_compute_crc() {
   constexpr uint8_t payload[] = {0xff, 0xff, 0xfd, 0x00, 0x01, 0x07, 0x00, 0x02, 0x84, 0x00, 0x04, 0x00};
 
-  auto crc = dxl::compute_crc(payload, sizeof(payload));
+  auto crc = dxl::compute_crc(0, payload, sizeof(payload));
   TEST_ASSERT_EQUAL_UINT16(0x151d, crc);
 }
